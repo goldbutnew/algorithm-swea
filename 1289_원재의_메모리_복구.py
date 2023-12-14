57763e79ac012f5b15a30516b5f1158a33487242
geumhyunlee <goldbutnew@gmail.com>
2023-12-14 09:28:10 +0900

    solved

---
+++ 1289_원재의_메모리_복구.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("input.txt")
+
+T = int(input())
+for tc in range(1, T+1):
+
+    origin = list(map(int, input()))
+    mistake = [0] * len(origin)
+
+    cnt = 0
+
+    for i in range(len(origin)):
+        if origin[i] != mistake[i]:
+            for k in range(i, len(origin)):
+                mistake[k] = origin[i]
+            cnt += 1
+                # break
+
+    print(f'#{tc} {cnt}')
+
+
